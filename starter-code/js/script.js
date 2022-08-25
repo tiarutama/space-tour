99082e6f332424dd47a2df5f3cbf2de8a73884f4
tiarutama <isangasteruz@gmail.com>
2022-08-25 14:58:43 +0700

    crew page

---
+++ starter-code/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("commander").onclick = function () {
+    location.href = "crew-commander.html";
+};
+document.getElementById("specialist").onclick = function () {
+    location.href = "crew-specialist.html";
+};
+document.getElementById("pilot").onclick = function () {
+    location.href = "crew-pilot.html";
+};
+document.getElementById("engineer").onclick = function () {
+    location.href = "crew-engineer.html";
+};
