cfe652940526daf905794c565fdb8b2dfdf246f5
tiarutama <isangasteruz@gmail.com>
2022-08-27 16:49:10 +0700

    technology page

---
+++ starter-code/js/technology.js
@@ -0,0 +1,9 @@
+document.getElementById("vehicle").onclick = function () {
+    location.href = "technology-vehicle.html";
+};
+document.getElementById("spaceport").onclick = function () {
+    location.href = "technology-spaceport.html";
+};
+document.getElementById("capsule").onclick = function () {
+    location.href = "technology-capsule.html";
+};
